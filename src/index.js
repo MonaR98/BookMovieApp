4b991bbd58dc1599891ee6f6b5a9f85c0c6f379e
MonaR98 <74354950+MonaR98@users.noreply.github.com>
2021-07-05 15:59:33 +0530

    Final Project

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import 'typeface-roboto';
-import registerServiceWorker from './registerServiceWorker';
-import Controller from './screens/Controller';
+import Controller from "./screens/Controller";
+import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(<Controller />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+  <React.StrictMode>
+    <Controller />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
